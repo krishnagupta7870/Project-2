ae7b225f33b9dc1df8b9a00c852cd7bff00467b3
Krishna Gupta <krishna.birgunj@gmail.com>
2025-11-23 15:40:34 +0545

    Update config.php

--- includes/config.php
+++ includes/config.php
@@ -1,16 +1,18 @@
 <?php 
-// DB credentials.
-define('DB_HOST','localhost');
-define('DB_USER','root');
-define('DB_PASS','');
-define('DB_NAME','bbdms');
-// Establish database connection.
-try
-{
-$dbh = new PDO("mysql:host=".DB_HOST.";dbname=".DB_NAME,DB_USER, DB_PASS,array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES 'utf8'"));
+define('DB_HOST','sql12.freesqldatabase.com');
+define('DB_USER','sql12809036');
+define('DB_PASS','gym9b65Bkc');
+define('DB_NAME','sql12809036');
+
+try {
+$dbh = new PDO(
+"mysql:host=".DB_HOST.";dbname=".DB_NAME,
+DB_USER,
+DB_PASS,
+array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES 'utf8'")
+);
 }
-catch (PDOException $e)
-{
+catch (PDOException $e) {
 exit("Error: " . $e->getMessage());
 }
 ?>
